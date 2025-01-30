e34252958319547fd3bcd87c5ddf531f5a01f834
lisadesai <lisadesai08@gmail.com>
2025-01-30 11:59:58 -0500

    Initial Commit

---
+++ NewApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NewApp
+//
+//  Created by Bellatrix Lestrange on 1/30/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NewApp/NewAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewAppApp.swift
+//  NewApp
+//
+//  Created by Bellatrix Lestrange on 1/30/25.
+//
+
+import SwiftUI
+
+@main
+struct NewAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
