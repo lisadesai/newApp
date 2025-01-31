fc633945ee68c6fc1adaf8601b7a8d6083459120
lisadesai <lisadesai08@gmail.com>
2025-01-31 11:36:30 -0500

    date pickers

--- NewApp/ContentView.swift
+++ NewApp/ContentView.swift
@@ -10,14 +10,14 @@ import SwiftUI
 struct ContentView: View {
     @State private var startDate = Date()
     @State private var endDate = Date()
+    
     var body: some View {
         VStack {
         
             HStack(spacing: 300){
                 Text("Start")
-                
                 Text("End")
-            
+
             }
             
             HStack{
@@ -35,10 +35,20 @@ struct ContentView: View {
                     .accentColor(.orange)
                  
             }.padding()
-            
-            
+ 
+            Text("Days used \(timeDiff(), specifier: "%0.f")")
+                .font(.title)
+                .padding()
         }
         .padding(10)
+        
+    }
+    
+    //Calculate diff in days, return as int.
+    func timeDiff() -> Double {
+        let cal = Calendar.current
+        let components = cal.dateComponents([.weekday], from: startDate, to: endDate)
+        return Double(components.day ?? 0)
     }
 }
 
