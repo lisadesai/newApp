07a0e223d617be3f49bfbc028877709c2903434d
lisadesai <lisadesai08@gmail.com>
2025-02-03 14:49:49 -0500

    views folder

--- NewApp/ContentView.swift
+++ NewApp/ContentView.swift
@@ -8,48 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var startDate = Date()
-    @State private var endDate = Date()
-    
     var body: some View {
-        VStack {
-        
-            HStack(spacing: 300){
-                Text("Start")
-                Text("End")
-
-            }
-            
-            HStack{
-                DatePicker(selection: $startDate,  displayedComponents: [.date], label: { Text("") })
-                    .datePickerStyle(CompactDatePickerStyle())
-                    .labelsHidden()
-                    .frame(maxWidth: .infinity)
-                    .padding()
-                    .accentColor(.orange)
-                DatePicker(selection: $endDate, displayedComponents: [.date], label: { Text("") })
-                    .datePickerStyle(CompactDatePickerStyle())
-                    .labelsHidden()
-                    .frame(maxWidth: .infinity)
-                    .padding()
-                    .accentColor(.orange)
-                 
-            }.padding()
- 
-            Text("Days used \(timeDiff(), specifier: "%0.f")")
-                .font(.title)
-                .padding()
-        }
-        .padding(10)
+        ZStack{
+            RoundedRectangle(cornerRadius: 20 ).fill(.white)
+                .shadow(radius: 20)
+                .frame(width: 400, height: 350)
+           
+            DatePicker(selection: /*@START_MENU_TOKEN@*/.constant(Date())/*@END_MENU_TOKEN@*/,displayedComponents: [.date] , label: { Text("Date") })
+                .datePickerStyle(GraphicalDatePickerStyle())
+                .labelsHidden()
+                .frame(maxWidth: .infinity)
+                .padding(10)
+                .accentColor(.green)
+        }.padding(20)
         
     }
-    
-    //Calculate diff in days, return as int.
-    func timeDiff() -> Double {
-        let cal = Calendar.current
-        let components = cal.dateComponents([.weekday], from: startDate, to: endDate)
-        return Double(components.day ?? 0)
-    }
+   
+   
 }
 
 #Preview {
