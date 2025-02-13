293dfaf6017f774b78e31fe5a592dadeea76e376
lisadesai <lisadesai08@gmail.com>
2025-02-13 15:31:09 -0500

    button broken

--- NewApp/ContentView.swift
+++ NewApp/ContentView.swift
@@ -9,18 +9,33 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack{
-            RoundedRectangle(cornerRadius: 20 ).fill(.white)
-                .shadow(radius: 20)
-                .frame(width: 400, height: 350)
-           
-            DatePicker(selection: /*@START_MENU_TOKEN@*/.constant(Date())/*@END_MENU_TOKEN@*/,displayedComponents: [.date] , label: { Text("Date") })
-                .datePickerStyle(GraphicalDatePickerStyle())
-                .labelsHidden()
-                .frame(maxWidth: .infinity)
-                .padding(10)
-                .accentColor(.green)
-        }.padding(20)
+        VStack{
+            ZStack{
+                RoundedRectangle(cornerRadius: 20 ).fill(.white)
+                    .shadow(radius: 7)
+                    .frame(width: 350, height: 350)
+                    .scaledToFit()
+                    
+                DatePicker(selection: /*@START_MENU_TOKEN@*/.constant(Date())/*@END_MENU_TOKEN@*/,displayedComponents: [.date] , label: { Text("Date") })
+                    .datePickerStyle(GraphicalDatePickerStyle())
+                    .labelsHidden()
+                    .frame(maxWidth: .infinity)
+                    .padding(10)
+                    .accentColor(.green)
+            }.padding(20)
+        }.containerRelativeFrame([.horizontal, .vertical])
+            .background(Color(red: 0.8196078431372549, green: 1.0, blue: 0.43529411764705883))
+        
+            
+            
+        
+        Button(action: {CreateTripView = true}) {
+            label: {
+                Text("Add a trip")
+                    .padding(20)
+                    .background(.green)
+                    .contentShape(Rectangle())
+            }
         
     }
    
